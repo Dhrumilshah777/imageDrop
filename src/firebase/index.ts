542f633d81016a2e910e9f80be0db233c4edbc7d
Dhrumil Shah 7 <dhrumilshah2225@gmail.com>
2025-10-07 14:56:34 +0000

    'use client';
    import { initializeApp, getApps, getApp, FirebaseApp } fr

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -15,29 +15,10 @@ export function initializeFirebase() {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
 
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
+  // If no app is initialized, initialize one. Otherwise, use the existing one.
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-    return getSdks(firebaseApp);
-  }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  return getSdks(app);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
